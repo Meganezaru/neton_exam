fc4b4e1683243309dfc0ec1f412ba49f4213c1ff
Kazuya Goto <kaag.fc@gmail.com>
2022-10-12 20:43:12 +0900

    再帰関数を定義

--- main.ts
+++ main.ts
@@ -3,7 +3,12 @@ export function add(a: number, b: number): number {
 }
 
 export function calc_max_value(limit_weight: number, weight: number[], value: number[]): number {
-  return 0
+  const item_count = weight.length;
+  function calc_value(limit_weight: number, index: number): number {
+    return 0
+  }
+
+  return calc_value(limit_weight, 0);
 } 
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
