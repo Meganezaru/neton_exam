722cce45d764b4d4741c4a8633ef53c1bdd13806
Kazuya Goto <kaag.fc@gmail.com>
2022-10-12 23:38:41 +0900

    キャッシュを追加して、再帰の重複を抑制する

--- main.ts
+++ main.ts
@@ -4,19 +4,33 @@ export function add(a: number, b: number): number {
 
 export function calc_max_value(limit_weight: number, weight: number[], value: number[]): number {
   const item_count = weight.length;
+
+  // 残り重量と残り商品数をインデックスにして結果をキャッシュ
+  let cache: {[key: number]:{[key: number]: number}} = {};
   function calc_value(limit_weight: number, index: number): number {
     if (item_count <= index) {
       // 商品をすべて確認した
       return 0
     }
 
+    if (limit_weight in cache && index in cache[limit_weight]) {
+      return cache[limit_weight][index];
+    }
+
     if (limit_weight < weight[index]) {
       return calc_value(limit_weight, index + 1);
     } else {
-      return Math.max(
+      const result = Math.max(
         value[index] + calc_value(limit_weight - weight[index], index + 1),
         calc_value(limit_weight, index + 1)
       )
+
+      if (limit_weight in cache) {
+        cache[limit_weight][index] = result;
+      } else {
+        cache[limit_weight] = {[index]: result};
+      }
+      return result;
     }
   }
 
