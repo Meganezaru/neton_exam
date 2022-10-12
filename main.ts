9652df345517e161da55b89708b5563102665ed2
Kazuya Goto <kaag.fc@gmail.com>
2022-10-12 21:07:54 +0900

    本実装

--- main.ts
+++ main.ts
@@ -5,7 +5,19 @@ export function add(a: number, b: number): number {
 export function calc_max_value(limit_weight: number, weight: number[], value: number[]): number {
   const item_count = weight.length;
   function calc_value(limit_weight: number, index: number): number {
-    return 0
+    if (item_count <= index) {
+      // 商品をすべて確認した
+      return 0
+    }
+
+    if (limit_weight < weight[index]) {
+      return calc_value(limit_weight, index + 1);
+    } else {
+      return Math.max(
+        value[index] + calc_value(limit_weight - weight[index], index + 1),
+        calc_value(limit_weight, index + 1)
+      )
+    }
   }
 
   return calc_value(limit_weight, 0);
